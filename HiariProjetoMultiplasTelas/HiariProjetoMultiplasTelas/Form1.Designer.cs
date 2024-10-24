f3a568dc49de55bda8e584acbb63c7348958d1b6
Hiarisoares <hiarilhysamariadasilva@gmail.com>
2024-10-24 16:53:49 -0300

    ProjetoMultiplasTelas
    Tenho que terminar as alterações

--- HiariProjetoMultiplasTelas/HiariProjetoMultiplasTelas/Form1.Designer.cs
+++ HiariProjetoMultiplasTelas/HiariProjetoMultiplasTelas/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonAdicionarProduto = new Button();
+            buttonDeletarProduto = new Button();
+            listBoxProdutos = new ListBox();
+            SuspendLayout();
+            // 
+            // buttonAdicionarProduto
+            // 
+            buttonAdicionarProduto.Location = new Point(12, 25);
+            buttonAdicionarProduto.Name = "buttonAdicionarProduto";
+            buttonAdicionarProduto.Size = new Size(119, 23);
+            buttonAdicionarProduto.TabIndex = 0;
+            buttonAdicionarProduto.Text = "Novo Produto";
+            buttonAdicionarProduto.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeletarProduto
+            // 
+            buttonDeletarProduto.Location = new Point(173, 25);
+            buttonDeletarProduto.Name = "buttonDeletarProduto";
+            buttonDeletarProduto.Size = new Size(112, 23);
+            buttonDeletarProduto.TabIndex = 1;
+            buttonDeletarProduto.Text = "Deletar Produto";
+            buttonDeletarProduto.UseVisualStyleBackColor = true;
+            buttonDeletarProduto.Click += button2_Click;
+            // 
+            // listBoxProdutos
+            // 
+            listBoxProdutos.FormattingEnabled = true;
+            listBoxProdutos.ItemHeight = 15;
+            listBoxProdutos.Location = new Point(12, 68);
+            listBoxProdutos.Name = "listBoxProdutos";
+            listBoxProdutos.Size = new Size(384, 214);
+            listBoxProdutos.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBoxProdutos);
+            Controls.Add(buttonDeletarProduto);
+            Controls.Add(buttonAdicionarProduto);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button buttonAdicionarProduto;
+        private Button buttonDeletarProduto;
+        private ListBox listBoxProdutos;
     }
 }
